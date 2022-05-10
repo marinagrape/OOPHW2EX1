e4d34d8d87ffdaafff4e0e420b1b2d084d926b52
marina <marinagrape02@gmail.com>
2022-05-10 23:35:55 +0300

    Добавьте файлы проекта.

---
+++ OOPHW2EX1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+//Constructor
+//1.Make a class Worker, which will have the following private fields
+//-name(name), salary(salary).Make sure that these properties are 
+//filled in the constructor when the object is created (like this: 
+//new Worker(name, age) ). Make also public methods getName, getSalary.
+
+//Create an object of this class 'John', age 25, salary 1000.
+//Display his age and salary.
+
+namespace OOPHW2EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker1 = new Worker("John",25, 1000);
+
+            worker1.GetName();
+            worker1.GetAge();
+            worker1.GetSalary();
+
+        }
+    }
+}
---
+++ OOPHW2EX1/Worker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Constructor
+//1.Make a class Worker, which will have the following private fields
+//-name(name), salary(salary).Make sure that these properties are 
+//filled in the constructor when the object is created (like this: 
+//new Worker(name, age) ). Make also public methods getName, getSalary.
+
+namespace OOPHW2EX1
+{
+    class Worker
+    {
+        private string _name;
+        private int _salary;
+        private int _age;
+
+
+        public Worker(string name, int age, int salary)
+        {
+            _name = name;
+            _salary = salary;
+            _age = age;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void GetName()
+        {
+            Console.WriteLine(_name);
+        }
+
+        public void SetSalary(int salary)
+        {
+            _salary = salary;
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine(_salary);
+
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public void GetAge()
+        {
+            Console.WriteLine(_age);
+
+        }
+    }
+}
